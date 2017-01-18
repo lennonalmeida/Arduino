ee5d8c5e4491e5f5c3ddd449cb065c0077392d2a
Lennon Almeida <lennon.roberto@outlook.com>
2017-01-18 15:45:43 -0200

    Add files via upload

---
+++ LCD_mostrando_texto.ino
@@ -0,0 +1,44 @@
+/*
+ * Mostrando texto e fazendo o scroll no lcd
+ */
+#include <LiquidCrystal.h>
+ 
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+ 
+void setup()
+{
+  lcd.begin(16, 2);
+  }
+ 
+void loop()
+{
+  
+  lcd.clear();
+  lcd.setCursor(7, 0);
+  lcd.print("Ola");
+  lcd.setCursor(7, 1);
+  lcd.print("Tudo bem?");
+  delay(1000);
+  
+  
+  scroll_left();
+  scroll_right();
+   
+ }
+ void scroll_left(){
+  for (int posicao = 0; posicao < 7; posicao++){
+    lcd.scrollDisplayLeft();
+    delay(500);
+  }
+ 
+ }
+  void scroll_right(){
+  for (int posicao = 0; posicao < 7; posicao++){
+    lcd.scrollDisplayRight();
+    delay(500);
+  }
+ 
+ }
+
+
+
