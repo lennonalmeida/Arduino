d8920eacfc3bbaf291ef9c04c6873a342181043b
Lennon Almeida <lennon.roberto@outlook.com>
2017-01-18 15:51:50 -0200

    Add files via upload

---
+++ ControlandoLedComPot.ino
@@ -0,0 +1,23 @@
+/*
+ * Controlando a frequencia do acender-desligar do led com o potenciometro
+ */
+
+#define LedHigh 8
+#define potenciometro 2
+int timewait = 0;
+
+void setup() {
+  pinMode(LedHigh, OUTPUT);
+  digitalWrite(LedHigh,HIGH);
+}
+
+void loop() {
+  timewait = analogRead(potenciometro);
+
+  digitalWrite(LedHigh, HIGH);
+  delay(timewait);
+  digitalWrite(LedHigh, LOW);
+  delay(timewait);
+
+
+}
