87664f72e8644597c7ef346b395764ece51f4c5e
Lennon Almeida <lennon.roberto@outlook.com>
2017-02-01 12:55:01 -0500

    Add files via upload

---
+++ SensorUltra.ino
@@ -0,0 +1,32 @@
+/*
+ * Sensor ultrasonico hc-sr04 medindo a distancia e ligando led
+ */
+#include <Ultrasonic.h>
+ 
+//Define os pinos para o trigger e echo
+#define trigger 4
+#define echo 5
+#define led 10
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(trigger, echo); //Para maiores informações, estudar o codigo da biblioteca Ultrasonic
+float distance = 0;
+void setup()
+{
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Lendo dados do sensor...");
+}
+ 
+void loop()
+{
+  //Exibe informacoes no serial monitor
+  distance = ultrasonic.Ranging(CM);
+  Serial.print("Distancia em cm: ");
+  Serial.println(distance);
+  if(distance < 20)
+      digitalWrite(led, 1);
+  else
+      digitalWrite(led, 0);
+      
+  delay(10);
+}
